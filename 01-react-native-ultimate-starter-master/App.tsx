1aeb6ff52c23f358634f204f013b8cb206b97060
TranDanHuy <danhuymsk04@gmail.com>
2025-07-22 14:58:24 +0700

    video 03

--- 01-react-native-ultimate-starter-master/App.tsx
+++ 01-react-native-ultimate-starter-master/App.tsx
@@ -2,11 +2,28 @@ import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  //data type
+//string
+  const [name, setName] = useState<string>("danhuyhehe");
+
+//number
+const [age,setAge] = useState<number>(30);
+//null, undefined, boolean
+const test = null;
+//object, array
+const [person, setPerson] = useState([{
+  name: "Huy",
+  age: 21
+}])
+
+
   return (
     <View style={styles.container}>
       <Text style={{fontSize:50, color: "pink"}}> Hello wolrd</Text>
      <View>
-      <Text style={styles.test1}>Xin chào</Text>
+      <Text style={styles.test1}>{name}</Text>
+      <Text style={styles.test1}>{JSON.stringify(person)}</Text>
       <Text style={styles.test2}> Tôi là Đan Huy</Text>
      </View>
 
@@ -18,9 +35,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    //alignItems: 'center',
+    //justifyContent: 'center',
     paddingHorizontal: 20,
+    paddingTop: 20,
   },
   test: {
     color: "red",
