018bc172d6a77d0ccb75ee5df885a9bc945e5098
TranDanHuy <danhuymsk04@gmail.com>
2025-07-23 15:12:25 +0700

    video 04

--- 01-react-native-ultimate-starter-master/App.tsx
+++ 01-react-native-ultimate-starter-master/App.tsx
@@ -17,6 +17,23 @@ const [person, setPerson] = useState([{
   age: 21
 }])
 
+const [todoList, setTodoList] = useState(
+  [
+    { id: 1, title: "Learn React Native" },
+    { id: 2, title: "Learn React.js" },
+    { id: 3, title: "Watching Netflix" },
+    { id: 4, title: "Playing ESport" },
+    { id: 5, title: "Subscribe Hỏi Dân IT :v" },
+    { id: 6, title: "Watching Youtube" },
+    { id: 7, title: "CR 7" },
+    { id: 7, title: "CR 8" },
+    { id: 7, title: "CR 9" },
+    { id: 7, title: "CR 10" },
+    
+
+  ]
+)
+
 
   return (
     <View style={styles.container}>
@@ -39,8 +56,14 @@ const [person, setPerson] = useState([{
       <Text style={styles.test1}>{name}</Text>
      
      </View>
-     <Button title='ADD NEW'/>
-      <TextInput style={styles.test2}> Tôi là Đan Huy</TextInput>
+     <Button title='ADD NEW' onPress={() => alert("click me")}/>
+      <View style={{marginTop: 20, borderColor: "red", borderWidth: 1}}>
+        {todoList.map(todo => {
+          return(
+            <Text style={styles.todo}>{todo.title}</Text>
+          )
+        })}
+      </View>
 
     </View>
   );
@@ -55,15 +78,23 @@ const styles = StyleSheet.create({
     fontSize: 60,
     color:"red",
     paddingTop: 20,
-    paddingHorizontal: 20
+    paddingHorizontal: 20,
+    marginTop: 50,
   },
   test: {
-    color: "red",
+    color: "blue",
   },
   test1: {
     color: "green",
   },
   test2: {
-    color: "red",
+    color: "blue",
+  },
+  todo: {
+    fontSize: 30,
+    backgroundColor: "yellow",
+    marginBottom: 20,
+    padding: 15,
+
   }
 });
