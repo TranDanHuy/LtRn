4ea9c9306fc93baa2a1b2aed834e070865d914bc
TranDanHuy <danhuymsk04@gmail.com>
2025-08-08 08:24:55 +0700

    video 07

--- 01-react-native-ultimate-starter-master/App.tsx
+++ 01-react-native-ultimate-starter-master/App.tsx
@@ -1,81 +1,42 @@
-import { useState } from 'react';
-import { Button, FlatList, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState } from "react";
+import {
+  Button,
+  FlatList,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import InputTodo from "./components/todo/input.todo";
+import ListTodo from "./components/todo/list.todo";
 
 export default function App() {
+  const [todoList, setTodoList] = useState<ITodo[]>([]);
 
-  //data type
-//string
-  const [name, setName] = useState<string>("a");
+  function randomInteger(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-//number
-const [age,setAge] = useState<number>(30);
-//null, undefined, boolean
-const test = null;
-//object, array
-const [person, setPerson] = useState([{
-  name: "Huy",
-  age: 21
-}])
-
-const [todoList, setTodoList] = useState(
-  [
-    { id: 1, title: "Learn React Native" },
-    { id: 2, title: "Learn React.js" },
-    { id: 3, title: "Watching Netflix" },
-    { id: 4, title: "Playing ESport" },
-    { id: 5, title: "Subscribe Hỏi Dân IT :v" },
-    { id: 6, title: "Watching Youtube" },
-    { id: 7, title: "CR 7" },
-    { id: 7, title: "CR 8" },
-    { id: 7, title: "CR 9" },
-    { id: 7, title: "CR 10" },
+  const addTodo = (text: string) => {
     
+    const todo = {id: randomInteger(1, 10000), title: text}
+    setTodoList([...todoList, todo]);
+  }
 
-  ]
-)
-
+  const deleteTodo = (id: number) => {
+    const newTodo =  todoList.filter(todo => todo.id != id);
+    setTodoList(newTodo)
+  }
 
   return (
     <View style={styles.container}>
-      
-     <View>
-      <TextInput 
-      onChangeText={value => setName(value)}
-      value={name}
-      autoCapitalize='none'
-      autoCorrect= {false}
-      //keyboardType='numeric'
-      //maxLength={2}
-      //multiline
-      style={{
-        
-        borderColor: "violet",
-        borderWidth: 1,
-        padding: 10,
-      }}/>
-      <Text style={styles.test1}>{name}</Text>
-     
-     </View>
-     <Button title='ADD NEW' onPress={() => alert("click me")}/>
-      <FlatList style={{marginTop: 20, borderColor: "red", borderWidth: 1}} 
-      data={todoList} 
-      keyExtractor={item => item.id + ""}
-      renderItem={({item}) => {
-        return(
-            <Text 
-            //key={item.id} 
-            style={styles.todo}>{item.title}</Text>
-          )
-      }}
+      <InputTodo addTodo={addTodo}/>
+      <ListTodo 
+      todoList={todoList}
+      deleteTodo={deleteTodo}
       />
-      {/*<ScrollView style={{marginTop: 20, borderColor: "red", borderWidth: 1}}>
-        {todoList.map(todo => {
-          return(
-            <Text key={todo.id} style={styles.todo}>{todo.title}</Text>
-          )
-        })}
-      </ScrollView>*/}
-
+      
     </View>
   );
 }
@@ -83,11 +44,11 @@ const [todoList, setTodoList] = useState(
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     //alignItems: 'center',
     //justifyContent: 'center',
     fontSize: 60,
-    color:"red",
+    color: "red",
     paddingTop: 20,
     paddingHorizontal: 20,
     marginTop: 50,
@@ -101,11 +62,4 @@ const styles = StyleSheet.create({
   test2: {
     color: "blue",
   },
-  todo: {
-    fontSize: 30,
-    backgroundColor: "yellow",
-    marginBottom: 20,
-    padding: 15,
-
-  }
 });
---
+++ 01-react-native-ultimate-starter-master/components/todo/input.todo.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { Button, StyleSheet, TextInput, View } from "react-native";
+
+const styles = StyleSheet.create({
+  todoInput: {
+    borderColor: "violet",
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 20,
+  },
+});
+
+interface IProps {
+    addTodo: (v:  string) => void;
+}
+const InputTodo = (props: IProps) => {
+    const {addTodo} = props;
+  const [name, setName] = useState<string>("");
+  const handleAddNewTodo = () => {
+    addTodo(name);
+
+  }
+  return (
+    <>
+      <View>
+        <TextInput
+          onChangeText={(value) => setName(value)}
+          value={name}
+          autoCapitalize="none"
+          autoCorrect={false}
+          //keyboardType='numeric'
+          //maxLength={2}
+          //multiline
+          style={styles.todoInput}
+        />
+        <Button title="ADD NEW" onPress={handleAddNewTodo} />
+      </View>
+    </>
+  );
+};
+export default InputTodo;
