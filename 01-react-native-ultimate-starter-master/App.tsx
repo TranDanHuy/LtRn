0546feae23b62cffee15e13d8660b373f806838a
TranDanHuy <danhuymsk04@gmail.com>
2025-08-11 03:37:42 +0700

    video 13

--- 01-react-native-ultimate-starter-master/App.tsx
+++ 01-react-native-ultimate-starter-master/App.tsx
@@ -9,7 +9,7 @@ import {
   View,
 } from "react-native";
 
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, useRoute } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { useNavigation } from "@react-navigation/native";
 
@@ -21,34 +21,40 @@ export default function App() {
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <Text>Home Screen</Text>
-        <View style={{marginVertical: 10}}>
+        <View style={{ marginVertical: 10 }}>
           <Button
             title="Go to detail"
             onPress={() => navigation.navigate("Details")}
           />
         </View>
-        <View style={{marginVertical: 10}}>
+        <View style={{ marginVertical: 10 }}>
           <Button
             title="Go user id = 1"
-            onPress={() => navigation.navigate("Details")}
+            onPress={() =>
+              navigation.navigate("Details", { userId: 1, name: "Huy" })
+            }
           />
         </View>
-        <View style={{marginVertical: 10}}>
+        <View style={{ marginVertical: 10 }}>
           <Button
             title="Go user id = 2"
-            onPress={() => navigation.navigate("Details")}
+            onPress={() =>
+              navigation.navigate("Details", { userId: 1, name: "Huy thứ hai" })
+            }
           />
         </View>
       </View>
     );
   }
   function DetailsScreen() {
+    const route: any = useRoute();
+
     const navigation: any = useNavigation();
 
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <Text>Details Screen</Text>
-        <Text>user id = ???</Text>
+        <Text>user id = {route.params.userId}</Text>
         <Button title="Go back Home" onPress={() => navigation.goBack()} />
       </View>
     );
