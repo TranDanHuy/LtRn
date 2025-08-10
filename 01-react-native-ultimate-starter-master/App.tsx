9153f3e4cf1564318ee74ddb8c18685d35e64b49
TranDanHuy <danhuymsk04@gmail.com>
2025-08-10 22:15:55 +0700

    video 12

--- 01-react-native-ultimate-starter-master/App.tsx
+++ 01-react-native-ultimate-starter-master/App.tsx
@@ -11,28 +11,52 @@ import {
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useNavigation } from "@react-navigation/native";
 
 export default function App() {
   const Stack = createNativeStackNavigator();
 
-  function HomeScreen() {
+  function HomeScreen(props: any) {
+    const navigation = props.navigation;
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <Text>Home Screen</Text>
+        <View style={{marginVertical: 10}}>
+          <Button
+            title="Go to detail"
+            onPress={() => navigation.navigate("Details")}
+          />
+        </View>
+        <View style={{marginVertical: 10}}>
+          <Button
+            title="Go user id = 1"
+            onPress={() => navigation.navigate("Details")}
+          />
+        </View>
+        <View style={{marginVertical: 10}}>
+          <Button
+            title="Go user id = 2"
+            onPress={() => navigation.navigate("Details")}
+          />
+        </View>
       </View>
     );
   }
   function DetailsScreen() {
+    const navigation: any = useNavigation();
+
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <Text>Details Screen</Text>
+        <Text>user id = ???</Text>
+        <Button title="Go back Home" onPress={() => navigation.goBack()} />
       </View>
     );
   }
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
+        <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
